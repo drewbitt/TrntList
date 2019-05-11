2a20c55e5acbce8d13748cc8ea8f76e3fbcdaf84
drewbitt <andrew@drewbitt.com>
2019-05-11 00:30:10 -0400

    Compile to Java 1.8

--- app/src/main/java/com/drewbitt/trntlist/ui/recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/drewbitt/trntlist/ui/recyclerview/RecyclerAdapter.kt
@@ -1,11 +1,6 @@
 package com.drewbitt.trntlist.ui.recyclerview
 
-import com.chad.library.adapter.base.BaseQuickAdapter
-import com.chad.library.adapter.base.BaseViewHolder
-import com.drewbitt.trntlist.MainFragment
-import com.drewbitt.trntlist.data.ViewModel
-import com.drewbitt.trntlist.data.model.TrntJson
+/*class RecyclerAdapter(private val mainFragment: MainFragment, val viewModel: ViewModel) :
+    BaseQuickAdapter<TrntJson, BaseViewHolder>(R.layout.trnt_item, viewModel) {
 
-class RecyclerAdapter(private val mainFragment: MainFragment, val viewModel: ViewModel) : BaseQuickAdapter<TrntJson, BaseViewHolder>() {
-
-}
+}*/
